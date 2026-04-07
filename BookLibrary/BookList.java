9dd285e9e1c5c602c004ecce3bf9981aeb929416
tuantutanghuynh <tanghuynhtuantuwork@gmail.com>
2026-04-07 09:43:03 +0000

    write metho add and show for Booklist

---
+++ BookLibrary/BookList.java
@@ -0,0 +1,38 @@
+package BookLibrary;
+
+public class BookList {
+    Book[] books;
+    int count = 0;
+
+    public BookList(){
+        books = new Book[20];
+    }
+
+    public void AddTextBook(){
+        TextBook b = new TextBook();
+        b.Input();
+        books[count]=b;
+        count ++;
+        System.out.println("Add textbook successfully");
+    }
+
+    public void AddNovel(){
+        Novel b = new Novel();
+        b.Input();
+        books[count] = b;
+        count ++;
+        System.out.println("Add novel successfully");
+    }
+
+    public void Show(){
+        if(count == 0){
+            System.out.println("There is no book yet!");
+        }
+        System.out.println("Book list: ");
+        for (int i= 0; i < count ; i++){
+            books[i].PrintInfo();
+        }
+
+    }
+
+}

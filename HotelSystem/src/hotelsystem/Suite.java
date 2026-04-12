4115f1fd070a5d8df10f2a49bbc28f7f46bd9e47
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-04-12 23:01:41 +0700

    write suite for hotel room

--- HotelSystem/src/hotelsystem/Suite.java
+++ HotelSystem/src/hotelsystem/Suite.java
@@ -4,10 +4,50 @@
  */
 package hotelsystem;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tangh
  */
-public class Suite {
+public class Suite extends Room {
+
+    int numRoom;
+
+    @Override
+    public void Input() {
+        Scanner sc = new Scanner(System.in);
+        super.Input();
+
+        while (true) {
+            System.out.print("Number of rooms in suite: ");
+            try {
+                numRoom = Integer.parseInt(sc.nextLine().trim());
+                if (numRoom <= 0) {
+                    throw new Exception("Number of rooms must be > 0");
+                }
+                break; 
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Please enter a valid integer.");
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+    }
     
+    public void PrintInfo(){
+        System.out.println("[SUITE]");
+        super.PrintInfo();
+        System.out.println("Room in suite: " + numRoom);
+    }
+
+    @Override
+    public String GetRoomType() {
+       return "Suite";
+    }
+    
+    public double CalcCost(int nights){
+        return pricePerNight * nights * 2.0;
+    }
 }
